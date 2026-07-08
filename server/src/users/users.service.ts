fee4c766ef51e94e395ca0bed45dfea9c303317f
Manuel Amado <auxiliar.sistemas@petrocasinos.com>
2026-07-08 17:10:03 -0500

    style: formatear users.service.ts

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@nestjs/common';
+ximport { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { Prisma, User } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async findAll(): Promise<User[]> {
     return this.prisma.user.findMany({
@@ -13,8 +13,16 @@ export class UsersService {
     });
   }
 
-  async findOne(username: string): Promise<User | null> {
-    return this.prisma.user.findUnique({ where: { username: username.toLowerCase() } });
+  async findOne(usernameOrEmail: string): Promise<User | null> {
+    const identifier = usernameOrEmail.toLowerCase();
+    return this.prisma.user.findFirst({
+      where: {
+        OR: [
+          { username: identifier },
+          { email: identifier },
+        ],
+      },
+    });
   }
 
   async findById(id: string): Promise<User | null> {
@@ -24,7 +32,7 @@ export class UsersService {
   async create(data: Prisma.UserCreateInput): Promise<User> {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(data.password, salt);
-    
+
     return this.prisma.user.create({
       data: {
         ...data,
